01d5796fc3651a0a3b85250039328bcf68f43caa
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2020-04-06 13:28:57 +0530

    update_01
    Added Asia Pacific AMI ID and user data

--- terra_ec2/ec2.tf
+++ terra_ec2/ec2.tf
@@ -1,9 +1,19 @@
 resource "aws_instance" "terra_ec2_isntance01" {
-    ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+    ami = "ami-0912f71e06545ad88"
+    instance_type = "t2.micro"
 
-  tags = {
-    Name = "Terraform Created Instance"
-  }
+    tags = {
+        Name = "Terraform Created Instance"
+        Type = "automated deployment using terraform"
+    }
+    user_data = <<-EOF
+                  #!/bin/bash
+                  yum update -y
+                  yum install httpd
+                  systemctl start httpd.service
+                  systemctl enable httpd.service
+                  echo "Hello, I am $(hostname -f)" > /var/www/html/index.html
+                  EOF
 
 }
+
