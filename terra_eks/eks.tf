2a27cc2b3bff26df6d289c86990e066741cb5e6d
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2020-04-09 17:55:32 +0530

    prototype_phase_commit

--- terra_eks/eks.tf
+++ terra_eks/eks.tf
@@ -2,6 +2,7 @@
 resource "aws_eks_cluster" "example" {
   name     = "example"
   role_arn = "${aws_iam_role.example.arn}"
+  
 
   vpc_config {
     subnet_ids = ["${aws_subnet.example1.id}", "${aws_subnet.example2.id}"]
